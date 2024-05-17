d0875471e2a2a5f29e3e3771a61a0d406f485ce3
bigstar9906 <bigstar9906@naver.com>
2024-05-17 16:52:57 +0900

    Test: error page

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -2,11 +2,18 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
     @RequestMapping("/")
     public String index() {
         return "index";
+    }
+
+    @ResponseBody
+    @RequestMapping("/error1")
+    public String error() {
+        return "error";
     }
 }
